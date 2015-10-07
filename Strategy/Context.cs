c0c67c8cf2aa485d5e8616241a3a3fce0dfba19e
Grigoryan Artem <artem.thebest@mail.ru>
2015-10-07 23:40:05 +0300

    Added Strategy

---
+++ Strategy/Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    class Context
+    {
+        private Strategy _strategy;
+
+        public Context(Strategy strategy)
+        {
+            _strategy = strategy;
+        }
+
+        public void ExecuteOperation()
+        {
+            _strategy.Algorithm();
+        }
+    }
+}
---
+++ Strategy/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Context a = new Context(new ConcreteStrategyA());
+            Context b = new Context(new ConcreteStrategyB());
+
+            a.ExecuteOperation();
+            b.ExecuteOperation();
+        }
+    }
+}
