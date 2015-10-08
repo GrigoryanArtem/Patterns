fff7e93c5f8ff794febb6ca214d692d13392047a
Grigoryan Artem <artem.thebest@mail.ru>
2015-10-08 21:58:25 +0300

    Added Factory

---
+++ Factory/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Factory factory = new ConcreteFactoryA();
+            Product product;
+
+            product = factory.CreateProduct();
+            Console.WriteLine("Created: {0}.", product.GetType());
+
+            factory = new ConcreteFactoryB();
+
+            product = factory.CreateProduct();
+            Console.WriteLine("Created: {0}.", product.GetType());
+        }
+    }
+}
