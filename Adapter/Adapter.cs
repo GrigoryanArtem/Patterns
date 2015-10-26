da408d42759cd6ed0d938ca19e91320bf3766255
Grigoryan Artem <artem.thebest@mail.ru>
2015-10-27 01:12:29 +0200

    Added adapter

---
+++ Adapter/Adapter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    class Adapter : Target
+    {
+        private Adaptee _adaptee;
+
+        public Adapter(Adaptee adaptee)
+        {
+            _adaptee = adaptee;
+        }
+
+        public override string Info()
+        {
+            return "It's " + _adaptee.GetType() + " info.";
+        }
+    }
+}
---
+++ Adapter/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Target t = new Adapter(new Adaptee());
+            Console.WriteLine("Info: {0}", t.Info());
+        }
+    }
+}
