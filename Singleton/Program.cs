fd0f27682588ec1ca9a51aa565b6cc74813ab27f
Grigoryan Artem <artem.thebest@mail.ru>
2015-10-27 00:13:58 +0200

    Added singleton

---
+++ Singleton/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static public void PrintValue(int value)
+        {
+            Console.WriteLine("Value: {0}", value);
+        }
+
+        static void Main(string[] args)
+        {
+            Singleton a = Singleton.Instance;
+
+            PrintValue(a.Count);
+            
+            a.Add();
+            PrintValue(a.Count);
+
+            Singleton b = Singleton.Instance;
+            PrintValue(b.Count);
+
+            b.Add();
+            PrintValue(a.Count);
+        }
+    }
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Singleton
+    {
+        private int count;
+
+        protected Singleton()
+        {
+            count = 0;
+        }
+
+        sealed private class SingletonCreated{
+            static private readonly Singleton instance = new Singleton();
+
+            static public Singleton Instance
+            {
+                get
+                {
+                    return instance;
+                }
+            }
+        }
+
+        static public Singleton Instance{
+            get
+            {
+                return SingletonCreated.Instance;
+            }
+        }
+
+        public void Add()
+        {
+            count++;
+        }
+
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+        }
+    }
+}
